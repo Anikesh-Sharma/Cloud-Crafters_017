3335099910b0cc25183567a1b4286ef2e18244df
PatilAshwini07 <patilashwini.5244@gmail.com>
2024-11-15 22:34:47 +0530

    changing navbar and footer styling

---
+++ RentEase/src/routes/AdminRoute.jsx
@@ -0,0 +1,12 @@
+import React  from 'react';
+import { useAuth } from '../Context/AuthContext';
+import { Outlet } from 'react-router-dom';
+
+export const AdminRoute = () => {
+    const { adminLogin } = useAuth();
+
+    return adminLogin ? <Outlet /> : <div>
+        <h1>Access Denied</h1>
+        <p>You are not authorized to view this page.</p>
+    </div>
+}
