d8039220a74df3bfb35f77d5f0f4aa8977a7154d
Itspradeepmaurya <111578064+Itspradeepmaurya@users.noreply.github.com>
2024-11-16 22:57:12 +0530

    carousel add kiya, styling changes in cards, Nav links change

---
+++ src/Components/AdminSidebar/AdminSidebar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+const AdminSidebar = () => {
+    return (
+        <div>
+            <button>
+                Add Property
+            </button>
+            <button>
+                Edit Property
+            </button>
+
+        </div>
+    )
+}
+
+export default AdminSidebar
